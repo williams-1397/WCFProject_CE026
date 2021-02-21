9b60cd38cc71bffe1516405b2b4dbfe16425baca
williams-1397 <50476105+williams-1397@users.noreply.github.com>
2021-02-22 02:53:16 +0530

    Web Client Commit
    Web Client Commit

---
+++ CurrencyWebClient/Home.aspx
@@ -0,0 +1,119 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="CurrencyWebClient.WebForm1" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Currency Conversion Service</title>
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+* {box-sizing: border-box;
+    font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    background-color:#F3F3F3;
+}
+
+input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+
+input[type=submit] {
+  background-color: #4CAF50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size:20px;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+.container {
+  border-radius: 5px;
+  padding: 20px;
+  border:solid 1px;
+}
+h3{
+    font-size:30px;
+    font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    padding-left:130px;
+}
+.Result{
+    text-align:center;
+    font-size:30px;
+}
+    .auto-style1 {
+        margin-left: 52px;
+    }
+    .auto-style3 {
+        margin-left: 55px;
+    }
+    .auto-style4 {
+        margin-left: 56px;
+    }
+    .auto-style5 {
+        width: 739px;
+    }
+    .auto-style6 {
+        height: 72px;
+    }
+</style>
+</head>
+<body style="margin-left: 307px; margin-right: 312px; margin-top: 76px; width: 915px;">
+
+<div class="container">
+  <form id="form1" runat="server">
+      <h3 class="auto-style5">Welcome to Currency Conversion Service</h3>
+      <hr/ >
+      <label for="Amount">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Enter Your Amount</label>
+      <asp:TextBox ID="Amount_TextBox1" runat="server" CssClass="auto-style4" Width="674px" ></asp:TextBox>
+      <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Select Your Local Currency:
+            <asp:DropDownList ID="Currency_DropDownList1" runat="server" CssClass="auto-style3" Width="700px">
+                <asp:ListItem Value="INR">₹ (INR - Indian Rupees)</asp:ListItem>
+                <asp:ListItem Value="USD">$( USD - US Dollars)</asp:ListItem>
+                <asp:ListItem Value="CAD">C$ (CAD - Canadian Dollars)</asp:ListItem>
+                <asp:ListItem Value="GBP">£ (GBP - Great Britain Pound)</asp:ListItem>
+                <asp:ListItem Value="YEN">¥ (YEN - Japanese Yen)</asp:ListItem>
+                <asp:ListItem Value="EURO">€ (EURO Europian Euro)</asp:ListItem>
+                <asp:ListItem Value="PKR">₨ (PKR - Pakistani Rupees)</asp:ListItem>
+                <asp:ListItem Value="YUAN">¥ (YUAN - Chinese Yuan)</asp:ListItem>
+                <asp:ListItem Value="NZD">$ (NZD - New Zeland Dollars)</asp:ListItem>
+                <asp:ListItem Value="AED">د.إ  (AED - Aram Emirites Dirhams)</asp:ListItem>
+                <asp:ListItem Value="RUB">₽ (RUB - Russian Rubble)</asp:ListItem>
+            </asp:DropDownList>
+      <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     Select Currency To be Converted:
+            <asp:DropDownList ID="Currency_DropDownList2" runat="server" CssClass="auto-style3" Width="700px">
+                <asp:ListItem Value="INR">₹ (INR - Indian Rupees)</asp:ListItem>
+                <asp:ListItem Value="USD">$( USD - US Dollars)</asp:ListItem>
+                <asp:ListItem Value="CAD">C$ (CAD - Canadian Dollars)</asp:ListItem>
+                <asp:ListItem Value="GBP">£ (GBP - Great Britain Pound)</asp:ListItem>
+                <asp:ListItem Value="YEN">¥ (YEN - Japanese Yen)</asp:ListItem>
+                <asp:ListItem Value="EURO">€ (EURO Europian Euro)</asp:ListItem>
+                <asp:ListItem Value="PKR">₨ (PKR - Pakistani Rupees)</asp:ListItem>
+                <asp:ListItem Value="YUAN">¥ (YUAN - Chinese Yuan)</asp:ListItem>
+                <asp:ListItem Value="NZD">$ (NZD - New Zeland Dollars)</asp:ListItem>
+                <asp:ListItem Value="AED">د.إ  (AED - Aram Emirites Dirhams)</asp:ListItem>
+                <asp:ListItem Value="RUB">₽ (RUB - Russian Rubble)</asp:ListItem>
+            </asp:DropDownList>
+    <asp:Button ID="Button1" runat="server"  Text="Convert" OnClick="Button1_Click" Width="705px" CssClass="auto-style1" />
+      <br />
+      <br />
+      <br />
+    <center class="auto-style6"><asp:Label ID="Result_Label1" runat="server" class="Result"></asp:Label></center>
+  </form>
+    <hr />
+   <center> NOTE : Exchange Rates might be diffrent in such case make sure rates before any doing transactions. </div></center>
+</body>
+</html>
---
+++ CurrencyWebClient/Home.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CurrencyWebClient
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (Amount_TextBox1.Text == null)
+            {
+                Result_Label1.Text = "No Amount Found";
+            }
+            else
+            {
+                double amount = Convert.ToDouble(Amount_TextBox1.Text);
+                string currency1 = Currency_DropDownList2.SelectedValue.ToString();
+                string currency2 = Currency_DropDownList1.SelectedValue.ToString(); 
+                CurrencyConversionService.CurrencyConversionServiceClient sc =
+                    new CurrencyConversionService.CurrencyConversionServiceClient();
+
+                if (Currency_DropDownList1.SelectedIndex == 0)
+                {
+                    double result = sc.ConvertINRTo(amount, currency1);
+                    Result_Label1.Text = "The Amount " + amount + Currency_DropDownList1.SelectedItem + " is equals to " + result.ToString() + Currency_DropDownList2.SelectedItem ;
+                }
+                if (Currency_DropDownList1.SelectedIndex == 1)
+                {
+                    double result = sc.ConvertUSDTo(amount, currency1);
+                    Result_Label1.Text = "The Amount " + amount + Currency_DropDownList1.SelectedItem + " is equals to " + result.ToString() + Currency_DropDownList2.SelectedItem;
+                }
+                if (Currency_DropDownList1.SelectedIndex == 2)
+                {
+                    double result = sc.ConvertCADTo(amount, currency1);
+                    Result_Label1.Text = "The Amount " + amount + Currency_DropDownList1.SelectedItem + " is equals to " + result.ToString() + Currency_DropDownList2.SelectedItem;
+                }
+                if (Currency_DropDownList1.SelectedIndex == 3)
+                {
+                    double result = sc.ConvertGBPTo(amount, currency1);
+                    Result_Label1.Text = "The Amount " + amount + Currency_DropDownList1.SelectedItem + " is equals to " + result.ToString() + Currency_DropDownList2.SelectedItem;
+                }
+                if (Currency_DropDownList1.SelectedIndex == 4)
+                {
+                    double result = sc.ConvertYENTo(amount, currency1);
+                    Result_Label1.Text = "The Amount " + amount + Currency_DropDownList1.SelectedItem + " is equals to " + result.ToString() + Currency_DropDownList2.SelectedItem;
+                }
+                if (Currency_DropDownList1.SelectedIndex == 5)
+                {
+                    double result = sc.ConvertEUROTo(amount, currency1);
+                    Result_Label1.Text = "The Amount " + amount + Currency_DropDownList1.SelectedItem + " is equals to " + result.ToString() + Currency_DropDownList2.SelectedItem;
+                }
+                if (Currency_DropDownList1.SelectedIndex == 6)
+                {
+                    double result = sc.ConvertPKRTo(amount, currency1);
+                    Result_Label1.Text = "The Amount " + amount + Currency_DropDownList1.SelectedItem + " is equals to " + result.ToString() + Currency_DropDownList2.SelectedItem;
+                }
+                if (Currency_DropDownList1.SelectedIndex == 7)
+                {
+                    double result = sc.ConvertYUANTo(amount, currency1);
+                    Result_Label1.Text = "The Amount " + amount + Currency_DropDownList1.SelectedItem + " is equals to " + result.ToString() + Currency_DropDownList2.SelectedItem;
+                }
+                if (Currency_DropDownList1.SelectedIndex == 8)
+                {
+                    double result = sc.ConvertNZDTo(amount, currency1);
+                    Result_Label1.Text = "The Amount " + amount + Currency_DropDownList1.SelectedItem + " is equals to " + result.ToString() + Currency_DropDownList2.SelectedItem;
+                }
+                if (Currency_DropDownList1.SelectedIndex == 9)
+                {
+                    double result = sc.ConvertAEDTo(amount, currency1);
+                    Result_Label1.Text = "The Amount " + amount + Currency_DropDownList1.SelectedItem + " is equals to " + result.ToString() + Currency_DropDownList2.SelectedItem;
+                }
+                if (Currency_DropDownList1.SelectedIndex == 10)
+                {
+                    double result = sc.ConvertRUBTo(amount, currency1);
+                    Result_Label1.Text = "The Amount " + amount + Currency_DropDownList1.SelectedItem + " is equals to " + result.ToString() + Currency_DropDownList2.SelectedItem;
+                }
+                if (Currency_DropDownList1.SelectedIndex == Currency_DropDownList2.SelectedIndex)
+                {
+                    Result_Label1.Text = "Invalid Selection.";
+                }
+            }
+            
+        }
+    }
+}
